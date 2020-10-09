f2a4b6d2862bc8478c0dcbcdba4df0a4894353ca
Rimanas <rimanas_alina@mail.ru>
2020-10-09 22:51:04 +0300

    Main.java added

---
+++ src/SaveMessage.java
@@ -0,0 +1,18 @@
+public class SaveMessage {
+    String authorMessage;
+    long phone;
+    String avatarUrl;
+    String recipientMessage;
+    String deviceType;
+    long created;
+    String typeMessage;
+    String photoUrl;
+    String videoUrl;
+    String gifUrl;
+    String audioUrl;
+    String smileUrl;
+    String content;
+    String sizeMessage;
+    boolean received;
+    boolean read;
+}
